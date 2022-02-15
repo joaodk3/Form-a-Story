8c1436c041e8111aaee79e11dbd755a43fc98355
João Victor Sales Santos <86613007+joaodk3@users.noreply.github.com>
2022-02-15 15:29:19 -0600

    Updated Readme file

---
+++ readme.mk
@@ -0,0 +1,2 @@
+A simple website to practice everything I have learned about introduction to <Form> concept in HTML.
+This was a guided project powered by Codecademy where I have used HTML, CSS and JavaScript concepts
